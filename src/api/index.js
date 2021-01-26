0da32495bcfc72b5d61da402a52a990b1b34e828
e-maubert <etienne.maubert@gmail.com>
2021-01-26 07:47:52 +0100

    Create api Marvel calls getCharacters and getCharacter

--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,38 @@
 import axios from 'axios';
-
+/*
 const instance = axios.create({
   baseURL: 'https://gateway.marvel.com/v1/public/',
   timeout: 1000,
 });
-
+*/
 const baseQueryParams = {
+  baseURL: 'https://gateway.marvel.com:443/v1/public/',
   apikey: process.env.REACT_APP_MARVEL_API_KEY,
 };
 
-const get = (url, query) => instance.get(url, {
-  params: {
-    ...query,
-    ...baseQueryParams,
-  },
-});
-
-export { instance as api, get };
+export default {
+  getCharacters: (offset, perPage) =>
+    axios({
+        'method':'GET',
+        'url':baseQueryParams.baseURL+`characters`,
+        'headers': {
+            'content-type':'application/octet-stream',
+        },
+        'params': {
+            'limit':perPage,
+            'offset':offset,
+            'apikey':baseQueryParams.apikey
+        },
+    }),
+  getCharacter: (id) =>
+    axios({
+      'method':'GET',
+      'url':baseQueryParams.baseURL+`characters/`+id,
+      'headers': {
+          'content-type':'application/octet-stream',
+      },
+      'params': {
+          'apikey':baseQueryParams.apikey
+      },
+  })
+}
