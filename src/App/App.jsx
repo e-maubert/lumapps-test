37c35b67aaa20fc03fecd7e24b3c73517d7e57d7
e-maubert <etienne.maubert@gmail.com>
2021-01-26 08:36:23 +0100

    Add routers Component Person and PersonList

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,31 +1,30 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
+
 import './App.scss';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from 'react-router-dom';
+
 import Header from '../components/Header';
 import Search from '../components/Search';
-import List from '../components/List';
+import PersonList from '../components/PersonList';
+import Person from '../components/Person';
 
 function App() {
   return (
-	<>
-		<Router>
-			<Header />
-			<Switch>
-				<Route
-					exact
-					path="/"
-				>
-					<Search />
-				</Route>
-			</Switch>
-		</Router>
-		<List />
-	</>
+	<Router>
+		<Header />
+		<Switch>
+			<Route exact path="/">
+				<Search />
+				<PersonList />
+			</Route>
+			<Route exact path="/person/:personId" component={Person} />
+		</Switch>
+	</Router>
   );
 }
 
