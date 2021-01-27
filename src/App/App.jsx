2712164a60f1e54536a7f9cde7bcc7e7a2010d39
e-maubert <etienne.maubert@gmail.com>
2021-01-27 11:00:51 +0100

    Create routes

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,31 +1,42 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from 'react';
+import React from 'react'
 
-import './App.scss';
+import './App.scss'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-} from 'react-router-dom';
+} from 'react-router-dom'
 
 import Header from '../components/Header';
-import Search from '../components/Search';
 import PersonList from '../components/PersonList';
 import Person from '../components/Person';
 
-function App() {
-  return (
-	<Router>
-		<Header />
-		<Switch>
-			<Route exact path="/">
-				<Search />
-				<PersonList />
-			</Route>
-			<Route exact path="/person/:personId" component={Person} />
-		</Switch>
-	</Router>
-  );
-}
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			search:''
+		}
+	}
+	
+	callbackSearch = (search) => {
+		this.setState({search: search})
+	}
 
-export default App;
+	render() {
+		console.log('this.state.search')
+		console.log(this.state.search)
+		return (
+			<Router>
+				<Header sendSearch={this.callbackSearch.bind(this)} />
+				<Switch>
+					<Route exact path="/">
+						<PersonList searchFromParent={this.state.search} />
+					</Route>
+					<Route exact path="/person/:personId" component={Person} />
+				</Switch>
+			</Router>
+		);
+	};
+}
